b5817722f29ee6b8df8409e93ee38b00a7753f81
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-01-16 17:20:57 -0300

    feat: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex justifyContent={"center"} alignItems={"center"} h={"100dvh"}>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
+        repellat officiis quidem corrupti aliquam error voluptates cum. Rerum
+        repudiandae dolor quo nulla repellat? Mollitia magni omnis eveniet
+        dolores repellendus quae.
+      </Text>
+    </Flex>
+  );
+}
