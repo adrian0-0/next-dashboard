ab3ae3bbd35fa9e10270f99e2343012dd8b3590e
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-01-17 12:18:10 -0300

    feat: next backend

--- src/pages/api/professor.ts
+++ src/pages/api/professor.ts
@@ -1,15 +1,44 @@
-// pages/api/professor.js
-import type { NextApiRequest, NextApiResponse } from "next";
-import { createRouter, expressWrapper } from "next-connect";
+import type { NextApiRequest, NextApiResponse } from "next/types";
+import { createRouter } from "next-connect";
 import { PrismaClient } from "@prisma/client";
+import { createSearchParamsBailoutProxy } from "next/dist/client/components/searchparams-bailout-proxy";
 
 const prisma = new PrismaClient();
 const professor = createRouter<NextApiRequest, NextApiResponse>();
 
-//R
-professor.get("/professor", async (request, response) => {
-  const getProfessorTab = await prisma.professor.findMany();
-  return response.status(200).json(getProfessorTab);
-});
+// Rota GET
+// professor.get(async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const getProfessorTab = await prisma.professor.findMany();
+//     return res.status(200).json(getProfessorTab);
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ error: "Ops, algo deu errado" });
+//   }
+// });
 
-export default professor;
+const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const getProfessorTab = await prisma.professor.findMany();
+    return res.status(200).json(getProfessorTab);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Ops, algo deu errado" });
+  }
+};
+
+export default GET;
+
+// pages/api/professor.js
+
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// type Data = {
+//   name: string;
+// };
+
+// const GET = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+//   res.status(200).json({ name: "John Doe" });
+// };
+
+// export default GET;
