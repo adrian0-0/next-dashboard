e9c705b0fee5a9306b4fa626b9e65b5e13e34606
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-01-26 12:40:47 -0300

    feat: test

--- src/pages/professor/index.tsx
+++ src/pages/professor/index.tsx
@@ -12,10 +12,15 @@ import {
   Heading,
   Button,
   Image,
+  Box,
+  Input,
+  Stack,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
-import Api from "@components/axiosApi";
 import { getData, postData } from "@components/crud";
+import router from "next/router";
 
 interface Professor {
   id: number;
@@ -26,7 +31,14 @@ interface Professor {
 
 function Professor() {
   const [professores, setProfessores] = useState<Professor[]>([]);
+  const [crudBox, setCrudBox] = useState(false);
+  const [crudHeadText, setCrudHeadText] = useState("");
+  const [submitVisibility, setSubmitVisibility] = useState(false);
   const professorPath = "/professor";
+  const crudBoxHeading = {
+    delete: "Deletar Professor",
+    edit: "Editar Professor",
+  };
   const errMessage = {
     get: "Erro: Na coleta de dados dos professores",
     post: "Erro: Na postagem do professor",
@@ -34,8 +46,10 @@ function Professor() {
     delete: "Erro Ao deletar um professor",
   };
 
-  function handleButton() {
-    // postData();
+  function handleButton(id: any, headingText: string) {
+    console.log(id);
+    setCrudBox(true);
+    setCrudHeadText(headingText);
   }
 
   useEffect(() => {
@@ -43,61 +57,85 @@ function Professor() {
   }, []);
 
   return (
-    <Flex justifyContent={"center"} alignItems={"center"} height={"100dvh"}>
-      <TableContainer>
-        <Heading textAlign={"center"}>Professores</Heading>
-        <Table variant="simple" mt={"2rem"}>
-          <Thead>
-            <Tr>
-              <Th></Th>
-              <Th></Th>
-              <Th></Th>
-              <Th>ID</Th>
-              <Th>Nome</Th>
-              <Th>Email</Th>
-              <Th>Celular</Th>
-            </Tr>
-          </Thead>
-          {professores.map((professor) => (
-            <Tbody>
-              <Tr key={professor.id}>
-                <Td>
-                  <Button>+</Button>
-                </Td>
-                <Td>
-                  <Image
-                    src="/assets/svg/pencil.svg"
-                    alt="Editar campo de texto"
-                  />
-                </Td>
-                <Td>
-                  <Image
-                    src="/assets/svg/trashcan.svg"
-                    alt="Excluir campo de texto"
-                  />
-                </Td>
-                <Td>{professor.id}</Td>
-                <Td>{professor.nome}</Td>
-                <Td>{professor.email}</Td>
-                <Td>{professor.celular}</Td>
+    <Box>
+      <Flex justifyContent={"center"} alignItems={"center"}>
+        <TableContainer>
+          <Heading textAlign={"center"}>Professores</Heading>
+          <Table variant="simple" mt={"2rem"}>
+            <Thead>
+              <Tr>
+                <Th></Th>
+                <Th></Th>
+                <Th></Th>
+                <Th>ID</Th>
+                <Th>Nome</Th>
+                <Th>Email</Th>
+                <Th>Celular</Th>
               </Tr>
-            </Tbody>
-          ))}
-          <Tfoot>
-            <Tr>
-              <Th></Th>
-              <Th></Th>
-              <Th></Th>
-              <Th>ID</Th>
-              <Th>Nome</Th>
-              <Th>Email</Th>
-              <Th>Celular</Th>
-            </Tr>
-          </Tfoot>
-          <TableCaption>Tabela do professor</TableCaption>
-        </Table>
-      </TableContainer>
-    </Flex>
+            </Thead>
+            {professores.map((professor) => (
+              <Tbody>
+                <Tr key={professor.id}>
+                  <Td>
+                    <Button>+</Button>
+                  </Td>
+                  <Td>
+                    <Image
+                      src="/assets/svg/pencil.svg"
+                      alt="Editar campo de texto"
+                      onClick={() =>
+                        handleButton(professor.id, crudBoxHeading.edit)
+                      }
+                    />
+                  </Td>
+                  <Td>
+                    <Image
+                      src="/assets/svg/trashcan.svg"
+                      alt="Excluir campo de texto"
+                    />
+                  </Td>
+                  <Td>{professor.id}</Td>
+                  <Td>{professor.nome}</Td>
+                  <Td>{professor.email}</Td>
+                  <Td>{professor.celular}</Td>
+                </Tr>
+              </Tbody>
+            ))}
+            <Tfoot>
+              <Tr>
+                <Th></Th>
+                <Th></Th>
+                <Th></Th>
+                <Th>ID</Th>
+                <Th>Nome</Th>
+                <Th>Email</Th>
+                <Th>Celular</Th>
+              </Tr>
+            </Tfoot>
+            <TableCaption>Tabela do professor</TableCaption>
+          </Table>
+        </TableContainer>
+      </Flex>
+      <Box
+        display={crudBox ? "initial" : "none"}
+        justifyContent="center"
+        alignItems="center"
+        mt="2rem"
+        w="100%"
+        mx={{ base: "1rem", md: "3rem", lg: "5rem" }}
+      >
+        <Stack spacing="1.5rem">
+          <Heading>{crudHeadText}</Heading>
+          <FormControl isRequired>
+            <FormLabel textAlign="left">First name</FormLabel>
+            <Input placeholder="First name" />
+          </FormControl>
+          <Button display={submitVisibility ? "initial" : "none"} w="300px">
+            Enviar
+          </Button>
+        </Stack>
+      </Box>
+    </Box>
   );
 }
 
